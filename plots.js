6cb5f19a37109502ce9e3ad55c9c55dde4b5fc59
tom2u <tbrodhead@2u.com>
2020-08-10 13:27:06 -0500

    12.1.3

--- plots.js
+++ plots.js
@@ -1,16 +1,21 @@
-// Define the x and y arrays in separate variables:
-var xArray = [1, 2, 3];
-var yArray = [10, 20, 30];
-
-// Use the x and y array variables as the values in a plotObject variable:
-var plotObject = { x: xArray, y: yArray };
-
-// Store the plotObject inside a plotArray:
-var plotArray = [plotObject];
-
-// Use the plotArray as the second argument of the Plotly.newPlot() function:
-Plotly.newPlot("plotArea", plotArray);
-
-//This is exactly the same as the original code:
-
-//Plotly.newPlot("plotArea", [{ x: [1, 2, 3], y: [10, 20, 30] }]);
+var trace = {
+  x: [
+    "nonalcoholic beer",
+    "nonalcoholic wine",
+    "nonalcoholic martini",
+    "nonalcoholic margarita",
+    "ice tea",
+    "nonalcoholic rum & coke",
+    "nonalcoholic mai tai",
+    "nonalcoholic gin & tonic",
+  ],
+  y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+  type: "bar",
+};
+var data = [trace];
+var layout = {
+  title: "'Bar' Chart",
+  xaxis: { title: "Drinks" },
+  yaxis: { title: "% of Drinks Ordered" },
+};
+Plotly.newPlot("plotArea", data, layout);
