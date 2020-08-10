8798a011c818e10f8d3bff40b70ef42ca00a8821
tom2u <tbrodhead@2u.com>
2020-08-10 13:36:21 -0500

    12.1.4

--- plots.js
+++ plots.js
@@ -1,5 +1,5 @@
 var trace = {
-  x: [
+  labels: [
     "nonalcoholic beer",
     "nonalcoholic wine",
     "nonalcoholic martini",
@@ -9,13 +9,38 @@ var trace = {
     "nonalcoholic mai tai",
     "nonalcoholic gin & tonic",
   ],
-  y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-  type: "bar",
+  values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+  type: "pie",
 };
 var data = [trace];
 var layout = {
   title: "'Bar' Chart",
-  xaxis: { title: "Drinks" },
-  yaxis: { title: "% of Drinks Ordered" },
 };
 Plotly.newPlot("plotArea", data, layout);
+
+/*
+var trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  mode: "markers",
+  type: "scatter",
+};
+
+var trace2 = {
+  x: [2, 3, 4, 5],
+  y: [16, 5, 11, 9],
+  mode: "lines",
+  type: "scatter",
+};
+
+var trace3 = {
+  x: [1, 2, 3, 4],
+  y: [12, 9, 15, 12],
+  mode: "lines+markers",
+  type: "scatter",
+};
+
+var data = [trace1, trace2, trace3];
+
+Plotly.newPlot("plotArea", data);
+*/
